5a597357f32a6a1d8580e04e6f474ebd6d28bccf
jacobmolby <jacob.moelby@gmail.com>
2024-10-24 15:17:49 +0200

    improve translations

--- src/locales/lv.ts
+++ src/locales/lv.ts
@@ -4,7 +4,7 @@ import { printValue, LocaleObject } from 'yup';
 
 // Based on https://github.com/jquense/yup/blob/b940eef48eb7456622ae384d0ffa7363d4fbad25/src/locale.ts
 export const mixed: LocaleObject['mixed'] = {
-  default: '${path} nav derīgs.',
+  default: '${path} nav derīgs',
   required: '${path} ir nepieciešamais lauks',
   defined: '${path} ir jādefinē',
   notNull: '${path} nevar būt nulle',
@@ -14,10 +14,10 @@ export const mixed: LocaleObject['mixed'] = {
     const isCast = originalValue != null && originalValue !== value;
     let msg =
       `${path} jābūt \`${type}\` tipam, ` +
-      `Bet galīgā vērtība bija: "${printValue(value, true)}\`` +
+      `bet galīgā vērtība bija: "${printValue(value, true)}\`` +
       (isCast
-        ? ` (Lieta no vērtības \`${printValue(originalValue, true)}\`).`
-        : '.');
+        ? ` (lieta no vērtības \`${printValue(originalValue, true)}\`)`
+        : '');
 
     if (value === null) {
       msg +=
@@ -33,12 +33,12 @@ export const string: LocaleObject['string'] = {
   length: '${path} jābūt tieši ${length} rakstzīmēm',
   min: '${path} jābūt vismaz ${min} rakstzīmēm',
   max: '${path} jābūt ne vairāk kā ${max} rakstzīmēm',
-  matches: '${path} Jāatbilst šādiem: "${regex}"',
+  matches: '${path} jāatbilst šādiem: "${regex}"',
   email: '${path} jābūt derīgam e -pastam',
   url: '${path} jābūt derīgam URL',
   uuid: '${path} jābūt derīgam UUID',
-  trim: '${path} Jābūt sagrieztai virknei',
-  lowercase: '${path} Jābūt mazajam virknei',
+  trim: '${path} jābūt sagrieztai virknei',
+  lowercase: '${path} jābūt mazajam virknei',
   uppercase: '${path} jābūt lielajam virknei',
 };
 
@@ -53,21 +53,21 @@ export const number: LocaleObject['number'] = {
 };
 
 export const date: LocaleObject['date'] = {
-  min: '${path} Laukam jābūt vēlāk nekā ${min}',
-  max: '${path} Laukam jābūt agrāk par ${max}',
+  min: '${path} laukam jābūt vēlāk nekā ${min}',
+  max: '${path} laukam jābūt agrāk par ${max}',
 };
 
 export const boolean: LocaleObject['boolean'] = {
-  isValue: '${path} Laukam jābūt ${value}',
+  isValue: '${path} laukam jābūt ${value}',
 };
 
 export const object: LocaleObject['object'] = {
   noUnknown:
-    '${path} Laukam nevar būt atslēgas, kas nav norādītas objekta formā',
+    '${path} laukam nevar būt atslēgas, kas nav norādītas objekta formā',
 };
 
 export const array: LocaleObject['array'] = {
-  min: '${path} Laukam jābūt vismaz ${min} priekšmetiem',
+  min: '${path} laukam jābūt vismaz ${min} priekšmetiem',
   max: '${path} laukam jābūt mazākam vai vienādam ar ${max} priekšmetiem',
   length: '${path} jābūt ${length} priekšmetiem',
 };
